db4d355b15a4d8954c52459bc0afa2f2013c1646
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-04-30 17:29:28 -0700

    cleaned-up Examples dir

---
+++ Examples/ControlStatements/DemoIf.scala
@@ -0,0 +1,8 @@
+// Demo evaluation of value passed as argument, using if statement
+if (args(0).value > 50) {
+	print("The number provided is greater than 50")
+}
+else {
+	print("The number provided is 50 or less")
+}
+
