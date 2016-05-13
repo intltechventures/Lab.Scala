4e4e39e5bd3ef48c8506dbbd960c41061a4d3f0e
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-05-12 22:42:12 -0700

    Updated DemoArray.scala

--- Examples/DataStructures/DemoArray.scala
+++ Examples/DataStructures/DemoArray.scala
@@ -1,3 +1,12 @@
+
+val pilots = new Array[String](3)
+pilots(0) = "Dick"
+pilots(1) = "TIm"
+pilots(2) = "Joe"
+for (pilot <- pilots)
+		println(pilot)
+
+
 val students = Array("Mike", "Bob", "Jane")
 for (student <- students)
 		println(student)
